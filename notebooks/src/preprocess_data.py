5ad46fc8359da1d9a2694c5f769957d4fb18f101
maw501 <maw501@gmail.com>
2020-12-07 19:05:00 +0000

    refactor preprocessing

--- notebooks/src/preprocess_data.py
+++ notebooks/src/preprocess_data.py
@@ -4,10 +4,6 @@ import pandas as pd
 pd.set_option("mode.chained_assignment", None)
 
 
-def load_data(file_path):
-    return pd.read_csv(file_path, parse_dates=["date"])
-
-
 def get_result_integer(string_result, position):
     return float(string_result.split("-")[position][0])
 
@@ -23,18 +19,21 @@ def get_result_non_integer(string_result, position):
         raise Exception("What the fluff.")
 
 
-def create_cols(df, mask, white_col, black_col, fave_name, underdog_name):
-    df[fave_name] = df.loc[mask, white_col]
-    df.loc[~mask, fave_name] = df.loc[~mask, black_col]
-    df[underdog_name] = df.loc[~mask, white_col]
-    df.loc[mask, underdog_name] = df.loc[mask, black_col]
+def load_df_drop_nas(file_path):
+    df = pd.read_csv(file_path, parse_dates=["date"])
+    return df.dropna()
 
 
-def process_all_data(file_path):
-    df = load_data(file_path)
-    df = df.dropna()
-    df["year"] = df["date"].dt.year
+def decide_when_p1_is_white(df):
+    df = df.copy()
+    np.random.seed(42)
+    p1_white = np.random.binomial(1, 0.5, size=len(df)) == 1
+    df["p1_white"] = p1_white
+    return df
 
+
+def get_scores_from_string(df):
+    df = df.copy()
     df["white_score"] = df["result"].apply(
         lambda x: get_result_integer(x, 0)
         if "/" not in x
@@ -45,81 +44,83 @@ def process_all_data(file_path):
         if "/" not in x
         else get_result_non_integer(x, 1)
     )
-    white_win = df["white_score"] > df["black_score"]
-    black_win = df["white_score"] < df["black_score"]
-    draw = df["white_score"] == df["black_score"]
-    df["outcome"] = 0
-    df.loc[white_win, "outcome"] = "white"
-    df.loc[black_win, "outcome"] = "black"
-    df.loc[draw, "outcome"] = "draw"
-    df["white_black_elo_diff"] = df["white_elo"] - df["black_elo"]
     return df
 
 
-def create_train_data(file_path):
-    df = process_all_data(file_path)
-    train = df.loc[df["date"] < "2019-01-01"]
+def create_single_p1_p2_col(df, suffix):
+    df = df.copy()
+    p1_white = df["p1_white"].values
+    df.loc[p1_white, f"p1_{suffix}"] = df.loc[p1_white, f"white_{suffix}"]
+    df.loc[~p1_white, f"p1_{suffix}"] = df.loc[~p1_white, f"black_{suffix}"]
 
-    train_ids = list(
-        set(train["white_id"].values).union(set(train["black_id"].values))
-    )
+    df.loc[p1_white, f"p2_{suffix}"] = df.loc[p1_white, f"black_{suffix}"]
+    df.loc[~p1_white, f"p2_{suffix}"] = df.loc[~p1_white, f"white_{suffix}"]
+    return df
 
-    train_id_to_avg_elo = (
-        train.groupby("white_id")["white_elo"].mean().to_dict()
-    )
 
-    train_id_to_avg_elo_ranked = {
-        k: v
-        for k, v in sorted(
-            train_id_to_avg_elo.items(), key=lambda item: item[1], reverse=True
-        )
-    }
+def create_p1_p2_cols(df):
+    df = df.copy()
+    suffixes = ["id", "elo", "title", "score"]
+    for suffix in suffixes:
+        df = create_single_p1_p2_col(df, suffix)
+    df["p1_id"] = df["p1_id"].astype(np.uint64)
+    df["p2_id"] = df["p2_id"].astype(np.uint64)
+    return df
 
-    ranking_dict = {
-        k: v
-        for k, v in zip(
-            train_id_to_avg_elo_ranked.keys(), range(1, len(train_ids) + 1)
-        )
-    }
 
-    train["white_prior_rank"] = train["white_id"].apply(
-        lambda x: ranking_dict[x]
-    )
-    train["black_prior_rank"] = train["black_id"].apply(
-        lambda x: ranking_dict[x]
-    )
-    white_fave = train["white_prior_rank"] < train["black_prior_rank"]
-
-    create_cols(
-        train, white_fave, "white_id", "black_id", "fave_id", "underdog_id"
-    )
-    create_cols(
-        train,
-        white_fave,
-        "white_prior_rank",
-        "black_prior_rank",
-        "fave_rank",
-        "underdog_rank",
-    )
-    create_cols(
-        train,
-        white_fave,
-        "white_score",
-        "black_score",
-        "fave_score",
-        "underdog_score",
+def get_id_to_elo_rating_dict(df):
+    elo_df = pd.concat(
+        [
+            df[["p1_id", "p1_elo"]].rename(
+                columns={"p1_id": "id", "p1_elo": "elo"}
+            ),
+            df[["p2_id", "p2_elo"]].rename(
+                columns={"p2_id": "id", "p2_elo": "elo"}
+            ),
+        ]
     )
 
-    train["fave_outcome"] = (1 + train["fave_score"] * 2).astype(int)
+    id_to_elo_rating_df = elo_df.groupby("id").mean().reset_index()
+    id_to_elo_rating_df = id_to_elo_rating_df.sort_values(
+        "elo", ascending=False
+    )
+    id_to_elo_rating_df["rank"] = range(1, len(id_to_elo_rating_df) + 1)
 
-    train["white_is_fave"] = (train["white_id"] == train["fave_id"]).astype(
-        int
+    return (
+        id_to_elo_rating_df[["rank", "id"]].set_index("id").to_dict()["rank"]
     )
 
-    train["abs_rank_diff"] = np.abs(
-        train["white_prior_rank"].values - train["black_prior_rank"].values
+
+def add_rank_cols(df, id_to_elo_rating_dict):
+    df = df.copy()
+    df["p1_prior_rank"] = df["p1_id"].apply(lambda x: id_to_elo_rating_dict[x])
+    df["p2_prior_rank"] = df["p2_id"].apply(lambda x: id_to_elo_rating_dict[x])
+
+    df["abs_rank_diff"] = np.abs(
+        df["p1_prior_rank"].values - df["p2_prior_rank"].values
     )
-    train["fave_underdog_score_diff"] = (
-        train["fave_score"].values - train["underdog_score"].values
+    return df
+
+
+def add_p1_outcome_col(df):
+    df = df.copy()
+    df["p1_outcome"] = (1 + df["p1_score"] * 2).astype(int)
+    return df
+
+
+def create_df(file_path):
+    df = load_df_drop_nas(file_path)
+    df = (
+        df.pipe(decide_when_p1_is_white)
+        .pipe(get_scores_from_string)
+        .pipe(create_p1_p2_cols)
     )
+    return df
+
+
+def process_train_df(train):
+    train = train.copy()
+    id_to_elo_rating_dict = get_id_to_elo_rating_dict(train)
+    train = add_rank_cols(train, id_to_elo_rating_dict)
+    train = add_p1_outcome_col(train)
     return train
